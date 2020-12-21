af418e25e483aa6eef6501d2808361f6d26d29fa
MaksimDemin <mdeminalex@icloud.com>
2020-12-21 14:32:13 +0200

    Begin use methods

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -2,22 +2,16 @@ package ru.job4j;
 
 public class Calculator {
 
-    public static void main(String[] args) {
-//        int one = 1;
-//        int two = 2;
-//        int six = 6;
-//        int four = 4;
-//        int five = 5;
-//        int sixDivTwo = six / two;
-//        int fiveMinusTwo = five - two;
-//        int fourTimeTwo = four * two;
-//        System.out.println(sixDivTwo);
-//        System.out.println(fiveMinusTwo);
+        public static void plus(int first, int second) {
+            int result = first + second;
+            System.out.println(result);
+        }
 
-        int size = 10;
-        size = 100;
-        size = size - 5;
-        System.out.println(size);
-    }
+        public static void main(String[] args) {
+            Calculator.plus(100, 500);
+            Calculator.plus(4, 2);
+            Calculator.plus(3, 5);
+
+        }
 
 }
