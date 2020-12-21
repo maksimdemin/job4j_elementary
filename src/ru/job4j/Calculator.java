56c550a371a567a358d63796515f10514f5e0e70
MaksimDemin <mdeminalex@icloud.com>
2020-12-21 14:01:19 +0200

    Init new var size and change this var

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -3,17 +3,21 @@ package ru.job4j;
 public class Calculator {
 
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+//        int one = 1;
+//        int two = 2;
+//        int six = 6;
+//        int four = 4;
+//        int five = 5;
+//        int sixDivTwo = six / two;
+//        int fiveMinusTwo = five - two;
+//        int fourTimeTwo = four * two;
+//        System.out.println(sixDivTwo);
+//        System.out.println(fiveMinusTwo);
+
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
     }
 
 }
